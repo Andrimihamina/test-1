60619a77f5c39942fa1be200e446ff72a14aafd1
Mihamina <andrymihamina7@gmail.com>
2024-04-30 15:39:49 +0300

    Troisieme commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ContactController;
+
+Route::get('users', [UsersController::class, 'create']);
+Route::post('users', [UsersController::class, 'store']);
+
+Route::get('contact', [ContactController::class, 'create']);
+Route::post('contact', [ContactController::class, 'store']);
